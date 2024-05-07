e18649bef1f849271f5de9be9aaee1c401b7577e
velpcode <velpmail@gmail.com>
2024-05-07 13:23:37 -0400

    aiBox

--- app/components/chatsupport.tsx
+++ app/components/chatsupport.tsx
@@ -1,11 +1,41 @@
 "use client"
-
+import { motion } from "framer-motion"
+import { cn } from "@/lib/utils"
+import { useState } from "react"
 
 export default function ChatSupport() {
 
+    const [isOpen, setOpen] = useState(false)
+
     return (
 
-        <div>ChatSupport</div>
+        <div>
+            <div className="fixed bottom-10 right-5 z-20">
+                <motion.div 
+
+                layout
+
+                animate={{ borderRadius: isOpen ? 10 : 50 }}
+                
+                initial = {{ borderRadius: 50 }}
+
+                className= {cn(
+                    "bg-neutral-300 h-[50px] w-[50px] rounded-lg shadow-sm border cursor-pointer",
+                    {
+                        "w-96 h-[34rem]":isOpen,
+                    }
+                    
+                    )}
+                    
+                    onClick={() => {
+                        setOpen(!isOpen);
+                    }}
+                    
+                    >
+                    
+                </motion.div>
+            </div>
+        </div>
 
     )
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ChatSupport from "./components/chatsupport";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="max-w-6xl min-h-screen mx-auto py-10 space-y-10 px-5 xl:px-0">
+          {children}
+        </main>
+        <ChatSupport />
+        </body>
+
     </html>
   );
 }
